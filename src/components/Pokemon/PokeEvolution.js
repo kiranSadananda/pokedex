6834aa2f06e745e7d1abcb6fc9a6c5fcd44a5a35
Kiran Sadananda <kiran.sadananda1994@gmail.com>
2022-05-10 12:42:35 -0400

    feat: evolution feature WIP

---
+++ src/components/Pokemon/PokeEvolution.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+import { GetImageById } from "../../functions/utils";
+
+const PokeEvolution = ({data, types}) => {
+    const [evolution, setEvolution] = useState();
+}
